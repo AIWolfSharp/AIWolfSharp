ac40b9963e82b168f9b041cacd21de812ed7ea5b
Takashi OTSUKI <otsuki@yz.yamagata-u.ac.jp>
2015-11-20 20:17:48 +0900

    IronPythonのプレイヤーを呼ぶ機能を実装

---
+++ PythonPlayer/PythonPlayer.py
@@ -0,0 +1,41 @@
+﻿import clr
+
+clr.AddReference("AIWolfLibCommon")
+from AIWolf.Common.Data import *
+from AIWolf.Common.Net import *
+
+class SimplePlayer(object):
+    me = -1
+
+    def get_Name(self):
+       return "SimplePlayer(IronPython)"
+
+    def Attack(self):
+        return Agent.GetAgent(self.me)
+
+    def DayStart(self):
+        pass
+
+    def Divine(self):
+        return Agent.GetAgent(self.me)
+
+    def Finish(self):
+        pass
+    
+    def Guard(self):
+        return Agent.GetAgent(self.me)
+
+    def Initialize(self, gameInfo, gameSetting):
+        self.me = gameInfo.Agent.AgentIdx
+
+    def Talk(self):
+        return Talk.OVER
+
+    def Update(self, gameInfo):
+        pass
+
+    def Vote(self):
+        return Agent.GetAgent(self.me)
+
+    def Whisper(self):
+        return Talk.OVER
