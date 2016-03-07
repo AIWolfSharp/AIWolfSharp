45a5d1d8aa20d7a8d2562e51a6b11ea5919aef68
AIWolfSharp <AIWolfSharp@users.noreply.github.com>
2016-03-07 19:42:57 +0900

    3ライブラリと2スターターの5プロジェクトのみに整理

--- NativeWrapper/NativeWrapper.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#using <mscorlib.dll>
-#using "CSWrapper.netmodule"
-
-using namespace System;
-using namespace System::Reflection;
-using namespace System::Runtime::InteropServices;
-using namespace AIWolf::NativePlayer;
